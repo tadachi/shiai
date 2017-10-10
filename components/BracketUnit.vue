dceec54dfcbab955530ccee748cd6308bae38d85
Takumi Adachi <takumiadachi@gmail.com>
2017-10-10 14:32:04 -0700

    Initial commit.

---
+++ components/BracketUnit.vue
@@ -0,0 +1,19 @@
+<template>
+  
+  <div>
+    <p><input type="text"></p>
+    <img src="~/assets/do.svg" width="80px">
+    <img src="~/assets/kote.svg" width="80px">
+    <img src="~/assets/men.svg" width="80px">
+    <img src="~/assets/edit-pencil.svg" width="80px">
+  </div>
+</template>
+<script>
+  module.exports = {
+    data: () => {
+      return {
+        greeting: 'Hello'
+      }
+    }
+  }
+</script>
