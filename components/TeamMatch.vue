19d14eeef7deae6c792d2cb3052e78bca834daef
Takumi Adachi <takumiadachi@gmail.com>
2017-11-02 19:38:20 -0700

    Added svg images for TeamMatch UI.

--- components/TeamMatch.vue
+++ components/TeamMatch.vue
@@ -1,13 +1,48 @@
 <template>
   <div v-if="show" class="section background modifiers">
     <div class="table-container">
-      <div class="table-cells-container" v-for="(n, key) in count" :key="key">
-        <div class="player-cell header-size">{{team_red_participants[key]}}</div>
-        <div class="points-cell standard-size"></div>
-        <div class="points-cell standard-size"></div>
-        <div class="player-cell header-size">{{team_white_participants[key]}}</div>
+      <div class="table-cells-container" v-for="(n, key) in (count/2)" :key="key">
+        <div class="player-cell dotted header-size">{{team_red_participants[key]}} {{key}}</div>
+        <div class="points-cell dotted standard-size" v-for="(n, k) in 2" :key="k">
+          <div else class="menu-right dotted">
+            <div class="wrap menu-item">
+              <img class="top-right super" src="~/static/add.svg">
+              <img class="icon" src="~/static/men.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="top-right super" src="~/static/add.svg">
+              <img class="icon" src="~/static/kote.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="top-right super" src="~/static/add.svg">
+              <img class="icon" src="~/static/dou.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="top-right super" src="~/static/add.svg">
+              <img class="icon" src="~/static/tsuki.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="top-right super" src="~/static/add.svg">
+              <img class="icon" src="~/static/penalty.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="icon" src="~/static/win.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="icon" src="~/static/lose.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="icon" src="~/static/tie.svg">
+            </div>
+            <div class="wrap menu-item">
+              <img class="icon" src="~/static/clear.svg">
+            </div>
+          </div>
+        </div>
+        <div class="player-cell dotted header-size">{{team_white_participants[key]}}</div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -30,6 +65,15 @@ export default {
 </script>
 
 <style scoped>
+.danger {
+  color: red;
+  font-size: 50px;
+}
+
+.dotted {
+  border-style: dotted;
+}
+
 .background {
   background-color: #161616;;
 }
@@ -53,18 +97,76 @@ export default {
 .player-cell {
   display: flex;
   background-color: grey;
-  margin: 10px;
 }
 
 .points-cell {
   display: flex;
   background-color: #e3e5e8;
-  margin: 10px;
 }
 
 .empty-cell {
   display: flex;
   background-color: black;
+}
+
+.menu-right {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  position: relative;
+  background-color: grey;
+  z-index: 9999;
+  opacity: 0;
+  left: 100%;
+  width: 50%;
+}
+
+.points-cell:hover {
+  background-color: grey;
+}
+
+.points-cell:hover .menu-right {
+  opacity: 1;
+}
+
+.points-cell:hover .menu-left {
+  opacity: 1;
+}
+
+.menu-item {
+  display: flex;
+  width: 50%;
+  height: 50px;
+  /* border-style: solid; */
+  border: 1px red solid;
+}
+
+.wrap {
+  position: relative;
+  cursor: pointer;
+  margin: 0px;
+  padding: 0px;
+}
+
+.wrap .top-right {
+  position: absolute;
+  /* Moves to to the right corner of image */
+  left: 70%;
+  top: 3%;
+  z-index: inherit;
+}
+
+.wrap .top-right-hidden {
+  position: absolute;
+  /* Moves to to the right corner of image */
+  left: 70%;
+  top: 3%;
+  z-index: inherit;
+  opacity: 0;
+}
+
+.wrap:hover .top-right-hidden {
+  opacity: 1;
 }
 
 @media screen and (min-width: 1024px) {
@@ -83,28 +185,13 @@ export default {
   }
 
   .icon {
-    width: 30px;
-    height: auto;
-  }
-
-  .small-icon {
-    width: 25px;
-    height: auto;
-  }
-
-  .medium-icon {
-    width: 60px;
-    height: auto;
-  }
-
-  .big-icon {
-    width: 70px;
+    width: auto;
     height: auto;
   }
 
   .super {
-    width: 11px;
-    height: 11px;
+    width: 25%;
+    height: 25%;
   }
 
 }
@@ -129,27 +216,14 @@ export default {
   }
 
   .icon {
-    width: 20px;
-
-  }
-
-  .small-icon {
-    width: 13px;
-  }
-
-  .medium-icon {
-    width: 32px;
-  }
-
-  .big-icon {
-    width: 42px;
+    width: auto;
+    height: auto;
   }
 
   .super {
-    width: 8px;
-    height: 8px;
+    width: 25%;
+    height: 25%;
   }
-
 }
 
 </style>
