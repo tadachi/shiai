e625441ad9dca27322e64b600bd4612385fb4a01
Takumi Adachi <takumiadachi@gmail.com>
2017-11-12 12:52:11 -0800

    Changed Round Robin to flexbox from of bulma columns

--- components/RoundRobin.vue
+++ components/RoundRobin.vue
@@ -1,88 +1,90 @@
 <template>
   <div v-if="show" class="section background modifiers">
-    <!-- <modal v-if="showModal"/> -->
-    <div class="columns is-mobile" v-for="(participant, n) in participants" :key="n">
-      <div class="column is-mobile-font-size is-paddingless is-centered is-narrow" v-for="(participant, m) in participants" :key="m">
-        <div class="header-size" v-if="n <= 0">
-          <h1 class="has-text-centered has-text-weight-bold has-text-white">{{participant}}</h1>
+    <div class="points-cell-container" v-for="(participant, n) in participants" :key="n">
+      <div v-for="(participant, m) in participants" :key="m">
+
+        <div v-if="n <= 0" class="header-size fill dotted is-mobile-font-size">
+          {{participant}}
         </div>
-        <div class="standard-size has-text-centered has-text-weight-bold has-text-white" v-else-if="m <= 0">
+
+        <div v-else-if="m <= 0" class="standard-size has-text-centered has-text-weight-bold has-text-white is-mobile-font-size dotted">
           <div>{{participants[n]}} ({{n}})</div>
           <div>{{score_card[n].wins}}W-{{score_card[n].losses}}L-{{score_card[n].ties}}T / {{score_card[n].points}}P</div>
         </div>
-        <div class="standard-size background-black-fill" v-else-if="m == n">
-        </div>
-        <div v-else :class="round_robin_card[n][m].outcome" class="standard-size fill expand has-text-white"> <!-- $store.state...outcome is win or lose. check .win and .lose class for more info -->
-          <div class="has-text-centered">
-            <div class="has-text-black">({{n}}, {{m}})</div>
-            <div class="fill-width-only">
-              <div class="wrap" v-for="(ippon, key) in round_robin_card[n][m].points" :key="key" v-on:click="removeIppon({x: n, y: m, index: key})">
-                <img class="top-right-hidden super" src="~/static/delete.svg">
-                <img class="medium-icon":src="ippon + '.svg'">
+
+        <div class="standard-size background-black-fill dotted" v-else-if="m == n"></div>
+          
+        <div v-else>
+          <div class="points-cell standard-size dotted" :class="round_robin_card[n][m].outcome" v-on:click="showPointsMenu('(' + m + ',' + n + ')')" >
+            <div class="score-menu">
+              <div class="has-text-black">({{n}}, {{m}})</div>
+              <div>
+                <div class="wrap score-item" v-for="(ippon, key) in round_robin_card[n][m].points" :key="key" v-on:click="removeIppon({x: n, y: m, index: key})">
+                  <img class="top-left-hidden super" src="~/static/delete.svg">
+                  <img class="" :src="ippon + '.svg'">
+                </div>
+              </div>
+              <div>
+                <div class="wrap penalty-item" v-for="(penalty, key) in round_robin_card[n][m].penalty" :key="key" v-on:click="removePenalty({x: n, y: m, index: key})">
+                  <img class="top-left-hidden super" src="~/static/delete.svg">
+                  <img class="":src="penalty + '.svg'">
+                </div>
+              </div>
+              <div>
+                <div class="wrap outcome-item" v-on:click="resetOutcome({x: n, y: m})">
+                  <img class="top-left-hidden super" src="~/static/delete.svg">
+                  <img class="" v-if="round_robin_card[n][m].outcome == 'win'" :src="round_robin_card[n][m].outcome + '.svg'">
+                  <img class="" v-if="round_robin_card[n][m].outcome == 'tie'" :src="round_robin_card[n][m].outcome + '.svg'">
+                </div>
               </div>
             </div>
-            <div class="fill-width-only">
-              <div class="wrap" v-for="(penalty, key) in round_robin_card[n][m].penalty" :key="key" v-on:click="removePenalty({x: n, y: m, index: key})">
-                <img class="top-right-hidden super" src="~/static/delete.svg">
-                <img class="small-icon":src="penalty + '.svg'">
+            <div class="points-menu standard-menu-size dotted" :ref="'(' + m + ',' + n + ')'">
+              <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: MEN})">
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/men.svg">
               </div>
-            </div>
-            <div class="wrap" v-on:click="resetOutcome({x: n, y: m})">
-              <img class="top-right-hidden super" src="~/static/delete.svg">
-              <img class="big-icon" v-if="round_robin_card[n][m].outcome == 'win'" :src="round_robin_card[n][m].outcome + '.svg'">
-              <img class="big-icon" v-if="round_robin_card[n][m].outcome == 'tie'" :src="round_robin_card[n][m].outcome + '.svg'">
+              <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: KOTE})">
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/kote.svg">
+              </div>
+              <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: DOU})">
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/dou.svg">
+              </div>
+              <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: TSUKI})">
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/tsuki.svg">
+              </div>
+              <div class="wrap menu-item" v-on:click="addPenalty({x: n, y: m, penalty: PENALTY})">
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/penalty.svg">
+              </div>
+              <div class="wrap menu-item" v-if="!round_robin_card[n][m].outcome" v-on:click="setWin({x: n, y: m, outcome: WIN})">
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/win.svg">
+              </div>
+              <div class="wrap menu-item" v-if="!round_robin_card[n][m].outcome" v-on:click="setLose({x: n, y: m, outcome: LOSE})">
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/lose.svg">
+              </div>
+              <div class="wrap menu-item" v-if="!round_robin_card[n][m].outcome" v-on:click="setTie({x: n, y: m, outcome: TIE})" >
+                <img class="top-left-hidden super" src="~/static/add.svg">
+                <img class="icon" src="~/static/tie.svg">
+              </div>
+              <div class="wrap menu-item" v-on:click="resetOutcome({x: n, y: m})" >
+                <img class="icon" src="~/static/clear.svg">
+              </div>
             </div>
           </div>
-          <div class="menu-left has-text-black rounded-corners">
-            <div>
-              <div class="has-text-centered">
-                <div class="has-text-black">[{{n}}, {{m}}]</div>
-                <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: MEN})">
-                  <img class="top-right super" src="~/static/add.svg">
-                  <img class="icon" src="~/static/men.svg">
-                </div>
-                <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: KOTE})">
-                  <img class="top-right super" src="~/static/add.svg">
-                  <img class="icon" src="~/static/kote.svg">
-                </div>
-                <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: DOU})">
-                  <img class="top-right super" src="~/static/add.svg">
-                  <img class="icon" src="~/static/dou.svg">
-                </div>
-                <div class="wrap menu-item" v-on:click="addIppon({x: n, y: m, ippon: TSUKI})">
-                  <img class="top-right super" src="~/static/add.svg">
-                  <img class="icon" src="~/static/tsuki.svg">
-                </div>
-                <div class="wrap menu-item" v-on:click="addPenalty({x: n, y: m, penalty: PENALTY})">
-                  <img class="top-right super" src="~/static/add.svg">
-                  <img class="icon" src="~/static/penalty.svg">
-                </div>
-                <div class="wrap menu-item" v-if="!round_robin_card[n][m].outcome" v-on:click="setWin({x: n, y: m, outcome: WIN})">
-                  <img class="icon" src="~/static/win.svg">
-                </div>
-                <div class="wrap menu-item" v-if="!round_robin_card[n][m].outcome" v-on:click="setLose({x: n, y: m, outcome: LOSE})">
-                  <img class="icon" src="~/static/lose.svg">
-                </div>
-                <div class="wrap menu-item" v-if="!round_robin_card[n][m].outcome" v-on:click="setTie({x: n, y: m, outcome: TIE})" >
-                  <img class="icon" src="~/static/tie.svg">
-                </div>
-                <div class="wrap menu-item" v-on:click="resetOutcome({x: n, y: m})" >
-                  <img class="icon" src="~/static/clear.svg">
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- {{$store.state.round_robin_card[n-1][m-1]}} -->
-        </div>
-      </div>
-    </div>
+        </div><!-- else -->
+      </div><!-- v-for :key="m" -->
+    </div><!-- container -->
   </div>
 </template>
 
 <script>
 import { MEN, KOTE, DOU, TSUKI, WIN, LOSE, TIE, PENALTY } from '../store/shiai_constants'
 import { ADD_IPPON, REMOVE_IPPON, SET_WIN, SET_LOSE, SET_TIE, RESET_OUTCOME, ADD_PENALTY, REMOVE_PENALTY } from '../store/round_robin'
-// import Modal from './Modal.vue'
 
 export default {
   data: () => {
@@ -137,6 +139,80 @@ export default {
 </script>
 
 <style scoped>
+@media screen and (min-width: 1px) {
+  .header-size {
+    min-width: 175px;
+    max-width: 175px;
+    min-height: 50px;
+    max-height: 50px;
+  } 
+
+  .standard-size {
+    min-width: 175px;
+    max-width: 175px;
+    min-height: 225px;
+    max-height: 225px;
+    margin: 0;
+    padding: 0;
+  }
+
+  .score-menu {
+    height: 150px;
+  }
+
+  .points-menu {
+    height: 75px;
+    background-color:grey;
+    opacity: 0;
+  }
+
+  .icon {
+    width: auto;
+    height: auto;
+  }
+
+  .score-icon {
+    width: 25%;
+    height: 25%;
+  }
+
+  .small-icon {
+    width: 10%;
+    height: 10%;
+  }
+
+  .super {
+    width: 25%;
+    height: 25%;
+  }
+
+  .score-item {
+    display: inline-block;
+    width: 20%;
+  }
+
+  .penalty-item {
+    display: inline-block;
+    width: 10%;
+  }
+
+  .outcome-item {
+    display: inline-block;
+    width: 20%;
+  }
+
+  .menu-item {
+    display: inline-block;
+    width: 20px;
+  }
+
+  .wrap .top-left-hidden {
+    opacity: 0;
+    position: absolute;
+    /* Moves to to the right corner of image */
+  }
+}
+
 .background {
   background-color: #161616;
 }
@@ -151,102 +227,44 @@ export default {
   overflow-x: auto;
 }
 
+.points-cell-container {
+  display: flex;
+  flex-direction: row;
+}
+
 .dotted {
   border-style: dotted;
 }
 
-.background-black {
-  background-color: black;
-  height: 100%;
-  width: 100%;
+.points-cell {
+  display: flex;
+  flex-direction: column;
+  background-color: #e3e5e8;
+  margin: 0;
+  padding: 0;
 }
 
-.rounded-corners {
-  border-radius: 5px;
+.points-cell:hover {
+  background-color: grey;
 }
 
-@media screen and (min-width: 1024px) {
-  .header-size {
-    min-width: 175px;
-    max-width: 175px;
-    min-height: 50px;
-    max-height: 550px;
-  } 
-
-  .standard-size {
-    min-width: 175px;
-    max-width: 175px;
-    min-height: 225px;
-    max-height: 550px;
-  }
-
-  .icon {
-    width: 30px;
-    height: auto;
-  }
-
-  .small-icon {
-    width: 25px;
-    height: auto;
-  }
-
-  .medium-icon {
-    width: 60px;
-    height: auto;
-  }
-
-  .big-icon {
-    width: 70px;
-    height: auto;
-  }
-
-  .super {
-    width: 11px;
-    height: 11px;
-  }
-
+.points-cell:hover .points-menu {
+  opacity: 1;
 }
 
-@media screen and (max-width: 1023px) {
-  .header-size {
-    min-width: 60px;
-    max-width: 60px;
-    min-height: 50px;
-    max-height: 550px;
-  } 
+.score-item:hover, .penalty-item:hover, .outcome-item:hover, .menu-item:hover {
+  opacity: 0.5;
+}
 
-  .is-mobile-font-size {
-    font-size: 9px;
-  }
+.wrap {
+  position: relative;
+  cursor: pointer;
+  margin: 0px;
+  padding: 0px;
+}
 
-  .standard-size {
-    min-width: 60px;
-    max-width: 60px;
-    min-height: 155px;
-    max-height: 550px;
-  }
-
-  .icon {
-    width: 20px;
-  }
-
-  .small-icon {
-    width: 10px;
-  }
-
-  .medium-icon {
-    width: 30px;
-  }
-
-  .big-icon {
-    width: 40px;
-  }
-
-  .super {
-    width: 8px;
-    height: 8px;
-  }
-
+.wrap:hover .top-left-hidden {
+  opacity: 1;
 }
 
 .fill {
@@ -265,100 +283,5 @@ export default {
   background-position:center center;
   background-color: #756d6d;
   z-index: -100;
-}
-
-.expand {
-  display: inline-block;
-  border-radius: 5px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-  -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-  transition: all 0.12s cubic-bezier(0.165, 0.84, 0.44, 1);
-}
-
-.expand::after {
-  border-radius: 5px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
-  opacity: 0;
-  -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-  transition: all 0.12s cubic-bezier(0.165, 0.84, 0.44, 1);
-}
-
-.expand:hover {
-  transform: scale(1.3, 1.5);
-  z-index: 100;
-}
-
-.expand:hover .menu-right {
-  opacity: 1;
-}
-
-.expand:hover .menu-left {
-  opacity: 1;
-}
-
-.menu-right {
-  position: absolute;
-  background-color: green;
-  z-index: inherit;
-  opacity: 0;
-  top: 0;
-  left: 100%;
-  width: 75%;
-  height: 100%;
-  border: 3px solid #73AD21;
-}
-
-.menu-left {
-  position: absolute;
-  background-color: #bec2c6;
-  z-index: inherit;
-  opacity: 0;
-  top: 0;
-  right: 100%;
-  width: 75%;
-  height: 100%;
-  min-width: 75%;
-  min-height: 100%;
-  max-width: 75%;
-  max-height: 100%;
-}
-
-.menu-item {
-  display: inline;
-}
-
-.wrap {
-  position: relative;
-  display: inline-block;
-  cursor: pointer;
-  margin: 0px;
-  padding: 0px;
-}
-
-.wrap .top-right {
-  position: absolute;
-  top: -1px;
-  /* Moves to to the right corner of image */
-  right: -1px;
-  z-index: inherit;
-}
-
-.wrap .top-right-hidden {
-  position: absolute;
-  top: -1px;
-  /* Moves to to the right corner of image */
-  right: 2px;
-  z-index: inherit;
-  opacity: 0;
-}
-
-.wrap:hover .top-right-hidden {
-  opacity: 1;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    build: {},
     postcss: {
       plugins: {
         'postcss-custom-properties': false
